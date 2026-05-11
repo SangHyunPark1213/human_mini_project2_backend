3f5429f3a74d5c9af7db9fc75dcc05b08a1e6946
admin <nare0527@gmail.coom>
2026-05-11 15:11:51 +0900

    별점 UI 수정

--- src/components/common/RatingStars.jsx
+++ src/components/common/RatingStars.jsx
@@ -1,14 +1,14 @@
 import "./RatingStars.css";
+import { FaStar } from "react-icons/fa";
 
 const RatingStars = ({ rating = 0, size = "md" }) => {
   return (
     <div className={`rating-stars rating-${size}`}>
       {[1, 2, 3, 4, 5].map((star) => (
         <span key={star} className={star <= Math.round(rating) ? "filled" : ""}>
-          ★
+          <FaStar />
         </span>
       ))}
-      <strong>{rating.toFixed(1)}</strong>
     </div>
   );
 };
