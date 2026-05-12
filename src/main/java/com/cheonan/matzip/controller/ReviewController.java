3e79bc5c28533dda036ab98db88260d27cf88840
admin <hyuna000905@gmail.com>
2026-05-12 16:26:47 +0900

    댓글수정삭제 추가

--- src/main/java/com/cheonan/matzip/controller/ReviewController.java
+++ src/main/java/com/cheonan/matzip/controller/ReviewController.java
@@ -14,12 +14,14 @@ import java.util.Map;
 public class ReviewController {
     private final ReviewService reviewService;
 
+    //리뷰 등록
     @PostMapping
     public ResponseEntity<String> createReview(@RequestBody ReviewRequest req) {
-        reviewService.createService(req);
+        reviewService.createReview(req);
         return ResponseEntity.ok("등록 완료");
     }
 
+    //리뷰 상호작용 like
     @PostMapping("/{reviewId}/interaction")
     public ResponseEntity<Map<String, String >>toggleInteraction(
             @PathVariable Long reviewId,
@@ -28,5 +30,27 @@ public class ReviewController {
 
                 String result = reviewService.toggleInteraction(reviewId, memberId, type);
                 return ResponseEntity.ok(Map.of("action", result));
+    }
+
+    //리뷰 수정
+    @PutMapping("/{reviewId}")
+    public ResponseEntity<String> updateReview(
+            @PathVariable Long reviewId,
+            @RequestParam Long memberId, //보통 세션이나 토큰에서 가져오지만 지금은 파라미터로 처리
+            @RequestBody ReviewRequest req) {
+
+        reviewService.updateReview(reviewId, memberId, req);
+        return ResponseEntity.ok("수정 완료");
+    }
+
+    //리뷰 삭제
+    @DeleteMapping("/{reviewId}")
+    public ResponseEntity<String> deleteReview(
+            @PathVariable Long reviewId,
+            @RequestParam Long memberId,
+            @RequestParam Long restaurantId) {
+
+        reviewService.deleteReview(reviewId, memberId, restaurantId);
+        return ResponseEntity.ok("삭제 완료");
     }
 }
--- src/main/java/com/cheonan/matzip/service/ReviewService.java
+++ src/main/java/com/cheonan/matzip/service/ReviewService.java
@@ -1,6 +1,7 @@
 package com.cheonan.matzip.service;
 
 import com.cheonan.matzip.dao.ReviewDao;
+import com.cheonan.matzip.dto.Review;
 import com.cheonan.matzip.dto.request.ReviewRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,9 @@ public class ReviewService {
 
     private final ReviewDao reviewDao;
 
+    //댓글 생성
     @Transactional
-    public void createService(ReviewRequest req) {
+    public void createReview(ReviewRequest req) {
         //리뷰 본문 저장, 생성된 리뷰 번호 받아오기
         Long reviewId = reviewDao.insertReview(req);
 
@@ -38,6 +40,62 @@ public class ReviewService {
         reviewDao.updateRestaurantReviewCount(req.getRestaurantId());
     }
 
+    //댓글 수정
+    //디비내에서는 수정된게 저장되면 태그와 사진이 전부 삭제된 뒤, 기존과 동일한 데이터를 다시 등록
+    @Transactional
+    public void updateReview(Long reviewId, Long currentMemberId, ReviewRequest req) {
+
+        //DB에서 작성자 ID를 가져와서 지금 유저(currentMemberId)와 비교
+        Long writerId = reviewDao.getReviewWriterId(reviewId);
+        if (!writerId.equals(currentMemberId)) {
+            throw new RuntimeException("수정 권한이 없습니다.");
+        }
+
+        //기본 내용 수정 및 기존 사진/태그 삭제
+        reviewDao.updateReview(req, reviewId);
+
+        //디비청소 (DELETE)
+        reviewDao.deleteReviewImages(reviewId);
+        reviewDao.deleteReviewSituations(reviewId);
+
+        //새로운 음식 사진 저장 (최대 3장), 수정 안할거면 유지
+        if (req.getFoodImageUrls() != null) {
+            int limit = Math.min(req.getFoodImageUrls().size(), 3);
+            for (int i = 0; i < limit; i++) {
+                String url = req.getFoodImageUrls().get(i);
+                reviewDao.insertReviewImage(reviewId, url);
+            }
+        }
+
+        //새로운 태그 저장, 수정 안할거면 유지
+        if (req.getSituations() != null) {
+            for (String situation : req.getSituations()) {
+                reviewDao.insertReviewSituation(reviewId, situation);
+            }
+        }
+
+        //평점 변동이 있을 수 있으니 식당 통계 갱신
+        reviewDao.updateRestaurantReviewCount(req.getRestaurantId());
+    }
+
+
+    //댓글 삭제
+    @Transactional
+    public void deleteReview(Long reviewId, Long currentMemberId, Long restaurantId) {
+        //DB에서 작성자 ID를 가져와서 지금 유저(currentMemberId)와 비교
+        Long writerId = reviewDao.getReviewWriterId(reviewId);
+        if (!writerId.equals(currentMemberId)) {
+            throw new RuntimeException("삭제 권한이 없습니다.");
+        }
+
+        //리뷰 본문 삭제, 이때 DB의 CASCADE 설정 덕분에 사진과 태그도 함께 삭제됨
+        reviewDao.deleteReview(reviewId);
+        //리뷰가 사라졌으니 식당의 리뷰 개수와 평균 평점을 즉시 갱신
+        reviewDao.updateRestaurantReviewCount(restaurantId);
+    }
+
+
+    //좋아요 도움되요
     @Transactional
     public String toggleInteraction(Long reviewId, Long memberId, String type) {
         //이미 눌렀는지 확인
