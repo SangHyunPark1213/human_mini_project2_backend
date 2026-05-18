ac7d7c532766ddb2f4d1465a4d3fec329c9cd481
SangHyunPark1213 <hjsh1213@naver.com>
2026-05-18 10:26:21 +0900

    localhost -> 크로스 설정 변경

--- src/main/java/com/cheonan/matzip/config/SecurityConfig.java
+++ src/main/java/com/cheonan/matzip/config/SecurityConfig.java
@@ -80,7 +80,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://192.168.0.169:3000"
+        ));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
